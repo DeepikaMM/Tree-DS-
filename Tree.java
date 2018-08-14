25f46860a4973a7e83e9ad998792fc895872c46c
Deepika Mohan <deepikamm12@gmail.com>
2018-08-14 20:29:32 +0530

    vertical view addedd

--- Tree.java
+++ Tree.java
@@ -1,6 +1,8 @@
 import java.util.*;
+import java.util.Map.Entry;
 public class Tree 
     {
+        Node root;
         public class Node
         {
             Node left;
@@ -13,49 +15,87 @@ public class Tree
                 
             }
         }
-        Node root;
-      
+
+        public void createWrapper(int key)
+        {
+            root = create(root,key);
+        }
+
+        public Node create(Node node, int key)
+        {
+            Node n = new Node(key);
+            if(node == null)
+                return n;
+            if(key > node.data)
+                node.right = create(node.right , key);
+            else if(key < node.data)
+                node.left = create(node.left,key);
+            return node; // return the node not 'n'
+        }
+          public void VerticalViewWrapper()
+        {
+             TreeMap<Integer, Vector<Integer>> treeset = new TreeMap<Integer,Vector<Integer>>();
+            VerticalView(treeset,0,root);
+             
+            for (Entry<Integer, Vector<Integer>> entry : treeset.entrySet())
+        {
+            System.out.println(entry.getValue());
+        }
+        }
+        public void VerticalView(TreeMap<Integer,Vector<Integer>> treeset,int count,Node node)
+        {
+            if(node==null)
+            return;
+            Vector<Integer> list = treeset.get(count);
+            if(list!=null)
+            {
+               
+                list.add(node.data);
+                treeset.put(count,list);
+            }
+            else
+            { 
+                 
+                 list = new Vector<Integer>();
+                list.add(node.data);
+              
+                treeset.put(count,list);
+                 
+            }
+            VerticalView(treeset,count+1,node.right);
+            VerticalView(treeset,count-1,node.left);
+        }
         public void inorder()
         {
             inorderWrapper(root);
         }
-        int i =0;
+
         public void  inorderWrapper(Node node)
         {
             if(node==null)
-            return;
-            i++;
+                return;
             inorderWrapper(node.left);
-            System.out.println("printing data " + i + node.data);
+            System.out.println(node.data);
             inorderWrapper(node.right);
         }
-        public  Node create(int key)
-        {
-            Node n = new Node(key);
-            if(root == null)
-            root = n;
-           
-            if(key > root.data)
-            root.right = create(root.right , key);
-            else if(key<root.data)
-            root.left = create(root.left,key);
-        
-            return n;
-        }
-        public void verticalview(Node node)
-        {
+      
 
-        }
         public static void main(String args[])
         {
             Scanner in =new Scanner(System.in);
             int n = in.nextInt();
             Tree t = new Tree ();
+           
             int arr[] = new int[n];
             for(int i=0;i<n;i++)
-            arr[i]  = in.nextInt();
-            for(int j=0;j<n;j++)
-            t.createWrapper(arr[j]);
-            t.inorder();
+                arr[i]  = in.nextInt();
+            for(int j=0;j<n;j++){
+                t.createWrapper(arr[j]);
+            }
+            System.out.println("\n");
+            //t.inorder();
+            t.VerticalViewWrapper();
+           
         }
     }
+
